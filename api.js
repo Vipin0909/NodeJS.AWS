7218e53bbff0e1465a37f39da0189874ac65c0e3
Vipin0909 <vipinmuley@gmail.com>
2020-10-19 19:40:07 +0530

    first aws commit

---
+++ api.js
@@ -0,0 +1,15 @@
+//------------------ Initialization of parameters-------------------------------------------
+const express     = require("express");
+const app         = express();
+
+// -----------------------Create Routes-----------------------
+
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+    res.send('Welcome Vipin on DevOps !');
+   });
+
+// -----------------Initializing the server port at 3000----------------------
+app.listen(3000, function() { 
+  console.log('Server listening on port 3000 for DevOps CI/CD integration testing...'); 
+});
