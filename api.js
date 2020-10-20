b0225246ac06a08c336e9ebe4f3bfbda202d016c
Vipin0909 <vipinmuley@gmail.com>
2020-10-20 20:12:58 +0530

    aws jenkins link test12

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ const app         = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-    res.send('Welcome Vipin on DevOps !%&!');
+    res.send('Welcome Vipin on DevOps');
    });
 
 // -----------------Initializing the server port at 3000----------------------
