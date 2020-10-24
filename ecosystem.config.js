6e2f50ddf36163c6a17ccaa04d0e53176e9137e9
Vipin0909 <vipinmuley@gmail.com>
2020-10-24 19:05:27 +0530

    updated code with new approach

--- ecosystem.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = { 
-    apps: [{
-      name    : "NodeJSDeployment",
-      script  : "npm",
-      args    : "start"
-    }]
-  }
--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-ssh ec2-user@3.17.66.184 <<EOF
+ssh ec2-user@3.17.66.184 <<'EOF'
     cd ~/NodeJSDeployment
     git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
@@ -8,5 +7,5 @@ ssh ec2-user@3.17.66.184 <<EOF
     nvm install v12.18.1
     npm install
     npm install -g nodemon pm2
-    pm2 restart ecosystem.config.js
-    exit
+    
+    PAUSE
