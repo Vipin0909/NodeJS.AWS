7e8fc95f83b0fc986ddaa4ab54ffadbf7fb814e5
Vipin0909 <vipinmuley@gmail.com>
2020-10-24 20:32:38 +0530

    CICD

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ const app         = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-    res.send('CICD' );
+    res.send('CICD Test' );
    });
 
 // -----------------Initializing the server port at 3000----------------------
