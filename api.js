ecffd53a506978f6450dfc8aea817e3ffb26a124
Vipin0909 <vipinmuley@gmail.com>
2020-11-03 18:07:26 +0530

    test CT

--- api.js
+++ api.js
@@ -7,7 +7,7 @@ const app         = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/login', function (req, res) {
-    res.send('Welcome to Login Page,we are on AWS Facebook' );
+    res.send('Welcome to Login Page,we are on AWS Facebook, we are working on CI CD CT' );
    });
 
    app.get('/aboutyou', function (req, res) {
