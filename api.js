b675fd66451c1ae37db81d833670d34afc4cfe02
Vipin0909 <vipinmuley@gmail.com>
2020-11-07 14:48:06 +0530

    localdocker

--- api.js
+++ api.js
@@ -3,7 +3,8 @@ var request = require('supertest');
 const express     = require("express");
 const app         = express();
 const mongoose    = require("mongoose");
-const apiuser = require("./apiusers");
+//const apiuser = require("./apiusers");
+const bankdata = require("./bankusers");
 
 //---------------------- Mongo DB connection settings--------------------------------------
 const MongoClient = require('mongodb').MongoClient;
@@ -11,14 +12,16 @@ const assert = require('assert');
 const { SSL_OP_TLS_BLOCK_PADDING_BUG } = require("constants");
 
 // Connection URL
-const url = ' mongodb+srv://yelpbdd:yelpadd@cluster0.4qboj.mongodb.net/myproject?retryWrites=true&w=majority';
+//const url = ' mongodb+srv://yelpbdd:yelpadd@cluster0.4qboj.mongodb.net/myproject?retryWrites=true&w=majority';
+const url = 'mongodb://localhost:27017';
 // Database Name
 const dbName = 'myproject';
 
-mongoose.connect(' mongodb+srv://yelpbdd:yelpbdd@cluster0.4qboj.mongodb.net/myproject?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://localhost/myproject', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+  useUnifiedTopology: true,
+}
+);
 
 // -----------------------Create Routes-----------------------
 
@@ -27,33 +30,60 @@ app.get('/login', function (req, res) {
     res.send('Welcome to Login Page,we are on AWS Facebook, we are working on CI CD CT AWESOME REALLY' );
    });
 
-// -----------------Serializaed Testing----------------------
-app.post('/posts/seri',async function(req,res){
-  try {
+// // -----------------Serializaed Testing----------------------
+// app.post('/posts/seri',async function(req,res){
+//   try {
     
-    const seri = new apiuser({
+//     const seri = new apiuser({
     
-      name:req.body.name,
-      language:req.body.language,
-      location:req.body.location,
-      laptop:req.body.laptop
+//       name:req.body.name,
+//       language:req.body.language,
+//       location:req.body.location,
+//       laptop:req.body.laptop
       
-  });
-  seri.save()
-  .then(data=> {
-    return res.json(data);
+//   });
+//   seri.save()
+//   .then(data=> {
+//     return res.json(data);
+//   })
+
+//   } catch (error) {
+//     res.json({message:error})
+//   }
+
+// });
+ 
+ // POST BANK the data to the database 
+ app.post('/post/bankusers',async function(req,res){
+  
+  //find an existing user
+  //let user = await bankuser.findOne({ email: req.body.email });
+  //if (user) return res.status(400).json({status:"User already registered."});
+  
+    const createbankuser = new bankdata({
+        useraccount:req.body.useraccount,
+        bank:req.body.bank,
+        branch:req.body.branch
+ 
+    });
+        
+    createbankuser.save()
+    .then(data=> {
+      
+      return res.json(data);
+    
+     
+    })
+    // .catch(err => {
+    //  // res.json({ message : "this is a message"})
+      
+    // });
   })
 
-  } catch (error) {
-    res.json({message:error})
-  }
-
-});
- 
    
 // -----------------Initializing the server port at at 8080----------------------
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
-  console.log('Server listening on port 8000 for AWS DevOps CI/CD integration testing.. . !');
+  console.log('Server listening on port 8080 with Docker .. . !');
 });
